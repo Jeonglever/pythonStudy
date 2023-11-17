e5c73e0e0adafd2ba262172ac6d248d1401ed967
정혜연 <jeonglever@gmail.com>
2023-11-17 13:35:23 +0900

    commit

--- practice8.py
+++ practice8.py
@@ -13,10 +13,50 @@
 # for waiting_no in range(5): # 0부터 5 미만까지 (0, 1, 2, 3, 4)
 #     print("대기번호 : {0}".format(waiting_no))
 
-for waiting_no in range(1, 6): # 시작번호와 끝번호를 지정 (6 직전까지)
-    print("대기번호 : {0}".format(waiting_no))
+# for waiting_no in range(1, 6): # 시작번호와 끝번호를 지정 (6 직전까지)
+#     print("대기번호 : {0}".format(waiting_no))
 
 
-starbucks = ["아이언맨", "토르", "아이엠 그루트"]
-for customer in starbucks:
-    print("{0}, 커피가 준비되었습니다!!")
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비되었습니다!!".format(starbucks))
+
+# while
+
+# customer = "토르"
+# index = 5
+#while (조건):
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1}번 남았어요.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 폐기처분 되었습니다.")
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}, 커피가 준비 되었습니다. 호출{1} 회".format(customer, index))
+#     index += 1
+# ↑ 무한루프에 빠지게 됨
+
+# customer = "토르"
+# person = "Unknown"
+
+# while person != customer:
+#     print("{0}, 커피가 준비 되었습니다.".format(customer))
+#     person = input("이름이 어떻게 되세요? ")
+
+# continue와 break
+
+# absent = [2, 5] #결석
+# no_book = [7] #책을 깜빡함
+# for student in range(1, 11): # 1~10번까지
+#     if student in absent:
+#         continue
+#     elif student in no_book:
+#         print("오늘 수업 여기까지. {0}는 교무실로 따라와!".format(student))
+#         break
+#     print("{0}, 책을 읽어봐".format(student))
+
+
+# 한 줄 for
